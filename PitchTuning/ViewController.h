a712814c6ca8d4f0c1d2426b69628d202fe11d23
Edward <Edward@Edwards-Mac-mini.local>
2013-11-26 11:07:08 +0800

    Initial Commit

---
+++ PitchTuning/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PitchTuning
+//
+//  Created by Edward on 26/11/13.
+//  Copyright (c) 2013 Edward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PitchTuning/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PitchTuning
+//
+//  Created by Edward on 26/11/13.
+//  Copyright (c) 2013 Edward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
